41d4ca14ce9f3ad312f1e0ffa56e6ae44472b681
kidde60 <kgeorgewilliam60@gmail.com>
2022-11-10 23:37:07 +0300

    add changes

--- morsecode.rb
+++ morsecode.rb
@@ -4,35 +4,35 @@ def objects
             "--." => "G", "...." => "H", ".." => "I", ".---" => "J",
             "-.-" => "K", ".-.." => "L", "--" => "M", "-." => "N",
             "---" => "O", ".--." => "P", "--.-" => "Q", ".-." => "R", "..." => "S", "-" => "T",
-            "..-" => "U", "...-" => "V", ".--" => "W", "-..-" => "X", "-.--" => "Y", "--.." => "Z" };
-  return codes;
+            '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z' }
+  return codes
 end
 
 # A method to decode MooseCode Character.
-print " Character code: ";
+print ' Character code: '
 def decod_char(s)
-  print objects()[s];
+  print objects()[s]
 end
-decod_char("---");
-print " Word Code: ";
+decod_char('---')
+print ' Word Code: '
 
 # Decoding a sentense
 def decode_word(word)
-  charArray = word.split(" ");
+  charArray = word.split(' ');
   for char in charArray
     decod_char(char)
   end
 end
-decode_word("-. .- -- .");
-print " Message Code: ";
+decode_word('-. .- -- .');
+print ' Message Code: ';
 
 # Decoding a Message
 def decode_message(message)
-  wordsArray = message.split("   ");
+  wordsArray = message.split('   ');
   for word in wordsArray
     decode_word(word)
-    print " "
+    print ' '
   end
 end
 
-decode_message(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...");
+decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...');
